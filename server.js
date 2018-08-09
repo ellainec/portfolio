b81bbc763ed6b0659c52cc0e83ed5c023039973f
ellainec <chan.ellaine@gmail.com>
2018-08-09 10:56:57 -0700

    cors package

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const creds = require('./config/config');
+const cors = require('cors');
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 app.post('/send', function(req, res) {
     let mailOpts, smtpTrans;
